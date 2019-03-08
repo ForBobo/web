2939c801d118fe5795ed22d7f2dabcee5fbd91aa
ForBobo <17637874258@163.com>
2019-03-08 16:37:31 +0800

    03.08

--- src/test/java/testGit.java
+++ src/test/java/testGit.java
@@ -1,16 +1,40 @@
-public class testGit {
-    public static void main(String[] args) {
 
-        System.out.println("HelloWorld");
-        String a="ab";
-        String b="cd";
-        String c=a+"cd";
-        String d="abcd";
-        String e=a+b;
-        String f="ab"+"cd";
-        System.out.println(c==d);
-        System.out.println(d==e);
-        System.out.println(c==e);
-        System.out.println(d==f);
+import org.junit.Test;
+
+import java.util.Scanner;
+
+public class testGit {
+
+    public static int a=6;
+    public static void main(String aaa [] ) {
+
+        int a;
+        int b=1;
+//        int c=a+b;
+
+//        test();
+//        System.out.println("HelloWorld");
+//        String a="ab";
+//        String b="cd";
+//        String c=a+"cd";
+//        String d="abcd";
+//        String e=a+b;
+//        String f="ab"+"cd";
+//        System.out.println(c==d);
+//        System.out.println(d==e);
+//        System.out.println(c==e);
+//        System.out.println(d==f);
+
+
     }
+//    @Test
+    public static void test(){
+        String s;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("shuru");
+         s= scanner.nextLine();
+        System.out.println(s);
+
+    }
+
 }
